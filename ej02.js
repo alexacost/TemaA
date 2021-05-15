b43bfa52f5aca2a212f244ab64c744258ccfe83e
alexacost <79877564+alexacost@users.noreply.github.com>
2021-05-15 19:29:31 -0300

    Ejercicio 02
    Ejercicio 02 terminado

---
+++ ej02.js
@@ -0,0 +1,9 @@
+var dolar = parseInt(prompt("¿Cuantos pesos equivalen a un dolar?"))
+var pesos = parseInt(prompt("¿Cuantos pesos tenés?"))
+var conver = pesos/dolar
+if (dolar >= 200) {
+    alert("Que la corrupción no destruya al pais! Viva MILEI 2023 | Viva el liberalismo 🗽")
+}
+ document.write("<p>Usted tiene "+ conver +" U$D</p>")
+
+
